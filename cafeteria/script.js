83c5d173f0126f0b11ffa2bd6c8051cea895a4d1
lvaldo <lvaldovinos@fi.uba.ar>
2023-10-05 20:34:01 -0300

    Added a JS validation

--- cafeteria/script.js
+++ cafeteria/script.js
@@ -1,7 +1,7 @@
 
 function validateDate() {
-    var userdate = new Date(document.getElementById("fecha").value).toJSON().slice(0,10);
-    var today = new Date().toJSON().slice(0,10);
-    if(userdate < today){
-      alert('No podes elegir una fecha anterior a hoy!');
+    var userdate = new Date(document.getElementById("fecha").value);
+    var today = new Date();
+    if(userdate <= today){
+      alert("No podes elegir una fecha anterior a hoy!");
 }
